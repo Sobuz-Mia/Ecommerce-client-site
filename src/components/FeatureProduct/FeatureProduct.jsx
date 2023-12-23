41fb6dbb1866e04b58c0a106948c5520a4578b70
Sobuz <sobuz.info0298@gmail.com>
2023-12-23 09:19:53 +0600

    product display

--- src/components/FeatureProduct/FeatureProduct.jsx
+++ src/components/FeatureProduct/FeatureProduct.jsx
@@ -1,10 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ProductCard from "../ProductCard/ProductCard";
 
 const FeatureProduct = () => {
-    return (
-        <div>
-            <h1 className="">Featured Products</h1>
-        </div>
-    );
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("/data.json").then((res) => setData(res.data));
+  }, []);
+  console.log(data);
+  return (
+    <div>
+      <h1 className="text-5xl text-center my-5">Featured Products</h1>
+      <div className="grid lg:grid-cols-4">
+        {data.map((item) => (
+          <ProductCard item={item} key={item.id}></ProductCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FeatureProduct;
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,24 @@
+const ProductCard = ({ item }) => {
+  const { title, image, variations } = item;
+  return (
+    <div className="border rounded overflow-hidden shadow-md transition-transform transform hover:scale-105 m-4">
+      <div className="p-4">
+        <img className="w-full h-56" src={image} alt={title} />
+        <h3 className="text-xl font-semibold mt-2 mb-4">{title}</h3>
+        <div className="flex flex-wrap">
+          <strong>Variations:</strong>
+          {variations.map((v) => (
+            <span
+              key={v.id}
+              className="bg-gray-200 text-gray-800 px-2 py-1 rounded m-1 text-sm"
+            >
+              {v.color} / {v.size}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
