6f9e15dc8ba906b98164f3923df2eec61f8abc0a
Sobuz <sobuz.info0298@gmail.com>
2023-12-23 10:01:40 +0600

    firebase setup done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtQn7amx-PpM8Pi8ZaBnFkccJff_Vdf7c",
+  authDomain: "ecommerce-project-55578.firebaseapp.com",
+  projectId: "ecommerce-project-55578",
+  storageBucket: "ecommerce-project-55578.appspot.com",
+  messagingSenderId: "639002032919",
+  appId: "1:639002032919:web:22c762b8a633507ea89552"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
